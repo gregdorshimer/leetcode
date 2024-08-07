c9153c067bba8a7ac06b13e04f9faf5efe83adc8
Greg Dorshimer <greg.dorshimer@protonmail.com>
2024-08-07 16:01:33 -0400

    commit

--- 443-string-compression.py
+++ 443-string-compression.py
@@ -4,21 +4,28 @@ def compress(chars):
     :rtype: int
     """
     s = ''
-    current = chars[0]
-    counter = 0
-    for char in chars:
-        if char == current:
+    counter = 1
+    for i in range(1, len(chars)):
+        current = chars[i]
+        prev = chars[i-1]
+        if current == prev:
             counter += 1
-            print('incremented counter to', counter)
         else:
-            print('s before changes:', s)
+            s += prev
+            if counter > 1:
+                s += str(counter)
+                counter = 1
+        if i == (len(chars) - 1):
             s += current
-            s += str(counter)
-            print('s after changes:', s)
-            current = char
-            counter = 0
-    print('s: ', s)
+            if counter > 1:
+                s += str(counter)
+
     chars = list(s)
     return len(chars)
 
+
+compress(['a', 'a', 'a', 'b', 'b', 'b', 'c'])
 compress(['a', 'b', 'c', 'c', 'c', 'c', 'c'])
+compress(['a', 'a', 'b', 'b', 'c', 'c'])
+compress(["a","a","b","b","c","c","c"])
+compress(['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'])

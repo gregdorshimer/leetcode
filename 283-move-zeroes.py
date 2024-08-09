e8a140d652398b23dd2d5c3816d0be576b080ea6
Greg Dorshimer <greg.dorshimer@protonmail.com>
2024-08-08 23:58:35 -0400

    commit

--- 283-move-zeroes.py
+++ 283-move-zeroes.py
@@ -3,4 +3,14 @@ def moveZeroes(nums):
     :type nums: List[int]
     :rtype: None Do not return anything, modify nums in-place instead.
     """
-    
+    i = 0
+    j = 0
+    while j < len(nums):
+        if nums[i] == 0:
+            nums.pop(i)
+            nums.append(0)
+            j += 1
+        else:
+            i += 1
+            j += 1
+

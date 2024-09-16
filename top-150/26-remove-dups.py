e1750b11fbc1928536b8f2c7395c9f366dff22d2
Greg Dorshimer <greg.dorshimer@protonmail.com>
2024-09-16 16:13:51 -0400

    commit

---
+++ top-150/26-remove-dups.py
@@ -0,0 +1,30 @@
+from os import remove
+
+
+def removeDuplicates(nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    i = 0
+    j = 1
+    if len(nums) == 0:
+        return 0
+    if len(nums) == 1:
+        return 1
+    while j < len(nums):
+        if i == j:
+            j += 1
+        elif nums[i] == nums[j]:
+            j += 1
+        elif nums[i] != nums[j] and i + 1 < j:
+            nums[i + 1] = nums[j]
+            i += 1
+            j += 1
+        else:
+            i += 1
+            j += 1
+    return i + 1
+
+print(removeDuplicates([1,1,2]))
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
